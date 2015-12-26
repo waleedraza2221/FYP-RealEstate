1b8f2310fb7c6aa0acd5c2010552f7dda3404a94
waleedraza2221 <waleedraza221@gmail.com>
2015-12-26 12:05:36 +0500

    First

---
+++ ELand/ELand/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ELand.Startup))]
+namespace ELand
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
